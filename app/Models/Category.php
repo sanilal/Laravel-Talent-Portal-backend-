db2a465cd233a121dcd6e98bf8607d6327af40c2
Sanilal <sanilal@gmail.com>
2025-10-01 09:41:23 +0400

    Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Category extends Model
 {
-    //
+    use HasUuids;
+
+    protected $fillable = ['name', 'slug', 'description'];
+
+    public function talents()
+    {
+        return $this->hasMany(User::class)->where('role', 'talent');
+    }
 }
