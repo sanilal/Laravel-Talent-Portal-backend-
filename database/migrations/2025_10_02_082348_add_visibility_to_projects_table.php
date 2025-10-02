3e7896859eefe093944e3e653ee60e5c8a30309e
Sanilal <sanilal@gmail.com>
2025-10-02 12:27:17 +0400

    add_visibility_to_projects_table

--- database/migrations/2025_10_02_082348_add_visibility_to_projects_table.php
+++ database/migrations/2025_10_02_082348_add_visibility_to_projects_table.php
@@ -6,23 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->enum('visibility', ['public', 'private', 'unlisted'])
+                ->default('public')
+                ->after('status');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropColumn('visibility');
         });
     }
 };
